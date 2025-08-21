fed063bb989ddbdf00d3c9ee2b218029e8657a21
Emanuel <emanuel132004@gmail.com>
2025-08-21 16:51:26 -0300

    Arrumado filtro de tags e aumento de velocidade de resposta

--- dataReceive.py
+++ dataReceive.py
@@ -2,46 +2,65 @@ import requests # type: ignore
 import json
 import re
 
-def gera_json(entrada_nome):
+def gera_json(entrada_nome: list):
     valores = {}
     nome = 'entradas.json'
-
-    for i in entrada_nome:
-        query = '''
-        query ($nome: String){
-            Media(type: MANGA, search: $nome, tag_not_in: ["Male Harem", "Age Gap", "Boys' Love", "Female Protagonist", "LGBTQ+ Themes", "Shoujo", "Office Lady", "Harem"]){
-                genres
-                chapters
-                volumes
-                format
-                description
-                countryOfOrigin
-                coverImage {
-                    medium
+    url = 'https://graphql.anilist.co'
+    query = '''
+            query ($nome: String){
+                Media(type: MANGA, search: $nome){
+                    genres
+                    chapters
+                    volumes
+                    format
+                    description
+                    countryOfOrigin
+                    coverImage {
+                        medium
+                    }
+                    tags {
+                        name
+                    }
                 }
             }
-        }
-        '''
+            '''
 
-        variables = {'nome': i}
-
-        url = 'https://graphql.anilist.co'
-
-        response = requests.post(url, json={'query': query, 'variables': variables})
-
-        if response.status_code == 200:
-            print("Sucesso na requisição!")
-            json_resposta = response.json()
-            valores[i] = json_resposta
-        else:
-            print(f"Erro de requisição. CODIGO: {response.status_code}")
-            json_erro = response.json()
-            with open("log_erro.json", 'w', encoding='utf-8') as log:
-                json.dump(json_erro, log, indent=4, ensure_ascii=False)
+    with requests.Session() as session:
+        for i in entrada_nome:
+            variables = {'nome': i}
+            response = session.post(url, json={'query': query, 'variables': variables})
+            if response.status_code == 200:
+                print("Sucesso na requisição!")
+                json_resposta = response.json()
+                valores[i] = json_resposta
+            else:
+                print(f"Erro de requisição. CODIGO: {response.status_code}")
+                json_erro = response.json()
+                with open("log_erro.json", 'w', encoding='utf-8') as log:
+                    json.dump(json_erro, log, indent=4, ensure_ascii=False)
 
     with open(nome, 'w', encoding='utf-8') as arquivo:
         json.dump(valores, arquivo, indent=4, ensure_ascii=False)
         arquivo.write("\n")
+
+
+
+def filtra_tags(entradas):
+    filtro = {"Male Harem", "Age Gap", "Boys' Love", "Female Protagonist", 
+              "LGBTQ+ Themes", "Shoujo", "Office Lady", "Harem"}
+
+    # dict comprehension otimizado
+    entradas_filtradas = {
+        nome_manga: dados
+        for nome_manga, dados in entradas.items()
+        if not filtro & {tag['name'] for tag in dados['data']['Media']['tags']}
+    }
+
+    return entradas_filtradas
+
+        
+
+
 
 def trata_entradas():
     print("\n--- Tratando os dados do arquivo JSON ---")
@@ -49,7 +68,7 @@ def trata_entradas():
     try:
         with open('entradas.json', 'r', encoding='utf-8') as arquivo:
             entradas = json.load(arquivo)
-            
+            entradas = filtra_tags(entradas)
             for nome_manga, dados_manga in entradas.items():
                 print(f"=========================================")
                 print(f"Tratando entrada: {nome_manga.upper()}")
